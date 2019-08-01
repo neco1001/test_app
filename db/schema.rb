e0f4883ec2dcb7f26a898964f7bbe1787c44f82c
Kenichi Kaneko <k1001kenichi@gmail.com>
2019-08-02 08:43:57 +0900

    Bookモデル追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_01_145139) do
+ActiveRecord::Schema.define(version: 2019_08_01_153846) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_books_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
